fb94062b13831c5dfb5804644eae77413684413a
Kjell <kjell@leanside.com>
2015-01-23 15:10:22 -0600

    check all files

--- check-description-and-credit.js
+++ check-description-and-credit.js
@@ -3,27 +3,33 @@ var spreadsheet = JSON.parse(fs.readFileSync('/dev/stdin').toString())
 var diffs = []
 
 spreadsheet.map(function(row) {
-  // || fs.existsSync('/Users/kolsen/Documents/TDXImages/'+row.original)
-  var file = '/Users/kolsen/Documents/TDXImages/'+row.renamed
-  if(row.renamed && fs.existsSync(file)) {
-    var diff = {file:row.renamed, desc: null, exifDesc: null, credit: null, exifCredit: null}
-      , exif = JSON.parse(fs.readFileSync(file+'.json'))
-      , _diff = false
 
-    if(row.description != exif.Description) {
-      diff.desc = row.description
-      diff.exifDesc = exif.Description
-      _diff = true
+  var dir = '/Users/kolsen/Documents/TDXImages/'
+    , files = ([row.original, row.renamed])
+
+  files.map(function(file) {
+    if(!file) return
+    var filePath = dir+file
+    if(fs.existsSync(filePath)) {
+      var diff = {file:file, desc: null, exifDesc: null, credit: null, exifCredit: null}
+        , exif = JSON.parse(fs.readFileSync(filePath+'.json'))
+        , _diff = false
+
+      if(row.description != exif.Description) {
+        diff.desc = row.description
+        diff.exifDesc = exif.Description
+        _diff = true
+      }
+      if(row.credit != exif.Credit) {
+        diff.credit = row.credit
+        diff.exifCredit = exif.Credit
+        _diff = true
+      }
+      if(_diff) {
+        diffs.push(diff)
+      }
     }
-    if(row.credit != exif.Credit) {
-      diff.credit = row.credit
-      diff.exifCredit = exif.Credit
-      _diff = true
-    }
-    if(_diff) {
-      diffs.push(diff)
-    }
-  }
+  })
 })
 
 console.log(JSON.stringify(diffs))
